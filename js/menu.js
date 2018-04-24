703a26b42ade83cdba0f904674cbf3d8bdf40b43
fxrl <f.rossel@gmx.de>
2018-04-24 20:51:36 +0200

    Made Menu
    added sprite for the flames.
    added menu bg image.
    added audio in the background.

--- js/menu.js
+++ js/menu.js
@@ -3,8 +3,13 @@
 var Menu = {
 
     preload : function() {
-        // Load all the needed resources for the menu.
+        //load background image
         game.load.image('menu', './assets/images/menu.png');
+        //load flame sprite sheet
+        game.load.spritesheet('flames', './assets/images/flames_sprite.png', 600, 221, 3);
+        //load menu audio
+        game.load.audio('laughter', './assets/sounds/devils_laugh.mp3');
+
     },
 
     create: function () {
@@ -12,8 +17,13 @@ var Menu = {
         // Add menu screen.
         // It will act as a button to start the game.
         var menuPage = this.add.button(0,0, 'menu', this.startGame, this);
-        menuPage.scale.setTo(0.5,0.5); 
-       
+        menuPage.scale.setTo(0.5);
+        var flames = game.add.sprite(0, 400, 'flames');
+        flames.scale.setTo(0.5);
+        flames.animations.add('fire', [0,1,2], 3, true);
+        flames.animations.play('fire');
+        var laughter = game.add.audio('laughter');
+        laughter.play();
 
     },
 
@@ -21,6 +31,7 @@ var Menu = {
 
         // Change the state to the actual game.
         this.state.start('Game');
+        game.sound.stopAll();
 
     }
 
