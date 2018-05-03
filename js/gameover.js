256b6f8098d161fbafa5c2573a5f95f87ba088b5
mliahim <mihail.gmul@gmail.com>
2018-05-03 14:55:15 +0200

    monsters and score
    I added the scoring system and created the monsters.

--- js/gameover.js
+++ js/gameover.js
@@ -10,9 +10,43 @@ var gameover = {
     create : function() {
 
         // Create button to start game like in Menu.
-        this.add.button(0, 0, 'gameover', this.startGame, this);
+      this.add.button(0, 0, 'gameover', this.startGame, this);
        var sound = this.add.audio('GOSound');
        sound.play();
+
+       //parse the saved scores
+       if (localStorage.topScores !== undefined) {
+           topScores = JSON.parse(localStorage.topScores);
+           //console.log(localStorage);
+        }
+
+        //top scores title
+        var highScoreText = game.add.text(150, 330, "Top Scores:", {
+          fontSize: "20px",
+          fill: '#4b657d',
+          align: "center",
+        });
+        highScoreText.anchor.set(0.5, 0.5);
+
+        //actual top scores
+        var yPos = 360;
+        for (var i = 0; i < topScores.length; i++) {
+            boop = game.add.text(150,yPos,topScores[i], {
+            fontSize: "20px",
+            fill: '#4b657d',
+            align: "center",
+          });
+          boop.anchor.set(0.5, 0.5);
+          yPos += 30
+        }
+
+        //last score text
+        var lastScoreText = game.add.text(150, 290, "Score: " + score, {
+            fontSize: "30px",
+            fill: '#4b657d',
+            align: "center",
+        });
+        lastScoreText.anchor.set(0.5, 0.5);
 
 
 
